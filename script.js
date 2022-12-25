849ebf686c95a51213a4259764b39f6148170803
jesper <logeshe9159@gmail.com>
2022-12-25 11:18:31 +0530

    added design in index.html

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// const apiconfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+//   measurementId: ""
+// };
 const apiconfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyCL8pUOlAL8i-gyWFW6jhjd2wYqyiQUKZA",
+  authDomain: "twoo-77302.firebaseapp.com",
+  databaseURL: "https://twoo-77302-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "twoo-77302",
+  storageBucket: "twoo-77302.appspot.com",
+  messagingSenderId: "135447477359",
+  appId: "1:135447477359:web:8acc4c56ab1e7714c4e19a",
+  measurementId: "G-WKRKWCHRWH"
 };
+
 
 firebase.initializeApp(apiconfig);
 const db = firebase.database();
